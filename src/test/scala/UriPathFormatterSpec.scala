72dc885cf2c26cab4360d64fa60e23a46f424c51
Maga Abdurakhmanov <maqdev@gmail.com>
2017-09-14 00:42:26 +0200

    + tests

--- src/test/scala/UriPathFormatterSpec.scala
+++ src/test/scala/UriPathFormatterSpec.scala
@@ -6,4 +6,9 @@ class UriPathFormatterSpec extends FlatSpec with Matchers{
     val s = "abc/{def}/ekl"
     UriPathFormatter.format(s, Map("def" → "100500")) shouldBe "abc/100500/ekl"
   }
+
+  it should "format string with parameters and text between" in {
+    val s = "abc/{def}:{klm}/ekl"
+    UriPathFormatter.format(s, Map("def" → "100500", "klm" → "yey")) shouldBe "abc/100500:yey/ekl"
+  }
 }
--- src/test/scala/UriPathParserSpec.scala
+++ src/test/scala/UriPathParserSpec.scala
@@ -11,13 +11,28 @@ class UriPathParserSpec extends FlatSpec with Matchers{
       SlashToken,
       TextToken("ekl")
     )
+  }
 
+  it should "parse string with parameters and text" in {
     val s2 = "abc/{def}~/ekl"
 
     UriPathParser.tokens(s2).toSeq shouldBe Seq(TextToken("abc"),
       SlashToken,
       ParameterToken("def"),
       TextToken("~"),
+      SlashToken,
+      TextToken("ekl")
+    )
+  }
+
+  it should "parse string with text between parameters" in {
+    val s = "abc/{def}:{klm}/ekl"
+
+    UriPathParser.tokens(s).toSeq shouldBe Seq(TextToken("abc"),
+      SlashToken,
+      ParameterToken("def"),
+      TextToken(":"),
+      ParameterToken("klm"),
       SlashToken,
       TextToken("ekl")
     )
